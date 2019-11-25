8f3184f1854add8691086277cf532a8e703e0ec4
dpzain <dpzain@163.com>
2019-11-26 00:25:55 +0800

    init~

---
+++ src/main/java/com/dpzain/albumdpzain/AlbumDpzainApplication.java
@@ -0,0 +1,13 @@
+package com.dpzain.albumdpzain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumDpzainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbumDpzainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dpzain/albumdpzain/AlbumDpzainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpzain.albumdpzain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumDpzainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
